84fef4fc1276fe8896537cd1651dd18d5dc6bac8
Makarr <maanromanov@edu.hse.ru>
2024-11-17 00:24:56 +0300

    set_user_kingdom_size() created

--- src/AppManager.cpp
+++ src/AppManager.cpp
@@ -1,4 +1,19 @@
 #include "AppManager.h"
+#include "utils.h"
 #include <iostream>
+using namespace std;
 
 
+void AppManager::set_user_kingdom_size() {    
+    string question = "Current size of kindom is "
+                            + to_string(AppManager::kingdom_size)
+                            + " * "
+                            + to_string(AppManager::kingdom_size)
+                            + "\nDo you want to change it?";
+    if (!is_answer_yes(question)){
+        return;
+    }
+    cout << "Enter new size for the kingdom!\n";
+    AppManager::kingdom_size = ask_int_value(MIN_KINGDOM_SIZE, MAX_KINGDOM_SIZE);
+    AppManager::set_user_kingdom_size();
+}
